61cc4b231e06433cc1a83f259b8f6bed4c48fcc2
bikrammehra <bikrammehra97@gmail.com>
2025-01-12 18:30:30 +0530

    updated app.py

--- app.py
+++ app.py
@@ -63,9 +63,6 @@ def setup_rag_chain(vector_store):
     perspectives on the user question, your goal is to help the user overcome some of the limitations of the \
     distance-based similarity search. Provide these alternative questions separated by newlines.
     Original question: {question}"""
-    
-    # template = """You are an AI assistant tasked with helping users find solutions to questions from English books. Given a user query, your goal is to generate five different versions of the question to help retrieve relevant documents from a vector database. By generating multiple perspectives on the user question, you help the user overcome the limitations of distance-based similarity search.
-    # Original question: {question}"""
 
     prompt = ChatPromptTemplate.from_template(template)
     
